9adc52c81760be5ecc88cbd9805d56241440ac9a
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2020-09-13 04:19:09 +0900

    create mock roulette

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,9 +3,8 @@
 describe("Roulette", () => {
   it("displays the candidate when there is only one candidate", () => {
     cy.visit("localhost:8080");
-    cy.get("#candidate").type("candidate1");
-    cy.get("#start").click();
-    cy.get("#stop").click();
-    cy.get("#result").should("contain", "candidate1");
+    cy.get("#candidate-area").type("candidate1");
+    cy.get("#pick").click();
+    cy.get("#result-area").should("contain", "candidate1");
   });
 });
